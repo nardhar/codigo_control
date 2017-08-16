f006652091e3ba9213ac6f2e0aa9bfccc49a4056
Nardhar Mure <nardhar.mure@gmail.com>
2017-08-16 00:41:58 -0400

    tabs fixed

--- groovy/src/main/groovy/org/nardhar/codigoControl/CodigoControl.groovy
+++ groovy/src/main/groovy/org/nardhar/codigoControl/CodigoControl.groovy
@@ -165,24 +165,24 @@ class CodigoControl {
   
   String toBase(Long numero, Integer base = 64) {
     def dic = [
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
-        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
-        'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 
-        'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-        'y', 'z', '+', '/'
-      ]
-      def cociente = 1
-      def resto = 0
-      def palabra = ''
-      while (cociente > 0) {
-        cociente = numero / base
-        resto = numero % base
-        palabra = dic[resto.toInteger()] + palabra
-        numero = cociente.toInteger()
-      }
-      palabra.size() > 1 ? palabra.replaceAll(/^0+(.*)/, '$1') : palabra
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+      'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+      'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 
+      'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+      'y', 'z', '+', '/'
+    ]
+    def cociente = 1
+    def resto = 0
+    def palabra = ''
+    while (cociente > 0) {
+      cociente = numero / base
+      resto = numero % base
+      palabra = dic[resto.toInteger()] + palabra
+      numero = cociente.toInteger()
+    }
+    palabra.size() > 1 ? palabra.replaceAll(/^0+(.*)/, '$1') : palabra
   }
 
   String verhoeffDigit(String value, Integer digits = 1) {
